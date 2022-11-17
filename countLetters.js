0da88d6fc84580d6ec9e854e55a8fb184baf9ed2
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-17 16:14:55 +0000

    CountLetters.js function returns a count of characters in a string, omitting spaces

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎟️ Assertion passed ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed ${actual} !=== ${expected}`);
+  }
+};
+
+
+const countLetters = function(sentence) {
+
+  let nuSentence = sentence.replaceAll(" ", "");
+  const countResults = {};
+
+  for (const char of nuSentence) {
+    if (countResults[char]) {
+      countResults[char] += 1;
+    } else {
+      countResults[char] = 1;
+    }
+  }
+  return countResults;
+  //console.table(countResults);
+};
+
+countLetters("lighthouse in the house");
+
+
