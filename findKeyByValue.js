4756718e0abea88aa9ef98dd50fe04a91a80159d
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-17 20:47:05 +0000

    created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,40 @@
+/*
+//test code (copied)
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎟️ Assertion passed ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion failed ${actual} !=== ${expected}`);
+  }
+};
+*/
+
+///goal: Search object and return first key that contains value
+const findKeyByValue = function(object, value) {
+
+  let found = '';
+
+  for (let property in object) {
+    if (object[property] === value) {
+      found = property;
+    }
+  }
+
+  console.log(found);
+
+};
+
+
+
+/* Test Code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire")
+findKeyByValue(bestTVShowsByGenre, "The Expanse")
+
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+*/
