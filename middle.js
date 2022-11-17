f80569060dcac25922a872035f6153e0ef041b29
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-17 00:13:58 +0000

    created middle function to find middle of even and odd numbered arrays

---
+++ middle.js
@@ -0,0 +1,60 @@
+const eqArrays = function(a, b) {
+  let returnME = false;
+
+  if (a.length !== b.length) {
+    return returnME;
+  }
+  if (a.every((value, index) => value === b[index])) {
+    returnME = true;
+  }
+  return returnME;
+};
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a,b)) {
+    console.log(`🎟️ Assertion passed ${a} === ${b}`);
+  } else {
+    console.log(`❌ Assertion failed ${a} !=== ${b}`);
+  }
+
+};
+
+// ACTUAL FUNCTION
+const middle = function(array) {
+
+  //empty array to hold middle values
+  let midArr = [];
+  
+  //Garud clause: no arrays smaller than 2
+  if (array.length < 3) return midArr;
+
+  //odd array, find middle then push that value and the one after it
+  if (array.length % 2 === 0) {
+    let midStart = array.length / 2;
+    midArr.push(midStart, midStart + 1);
+    //console.log(midArr)
+    return midArr;
+    
+  }
+
+  //even array, find middle by adding one to length and dividing by 2
+  if (array.length % 2 !== 0) {
+    let midStart = (array.length + 1) / 2;
+    midArr.push(midStart);
+    //console.log(midArr)
+    return midArr;
+  }
+
+};
+
+
+//tests
+middle([1]);// => []
+middle([1, 2]); // => []
+
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+
+
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
