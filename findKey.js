27855d247880b3dc98f7b102fe1c8610b15c4706
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-19 19:02:08 +0000

    implimented findKey function, if time come back to refactor using arrow function and if statement

---
+++ findKey.js
@@ -0,0 +1,55 @@
+
+const findKey = function (object, cb) {
+
+  for (let keys in object) {
+    if(typeof object[keys] === object) {
+      findKey(object[keys], cb)
+    } else {
+      if (cb(object[keys])) {
+        return keys
+      }
+    }
+  }
+}
+
+
+/* tried to refactor it using arrow functions and turnery opperator, how to put if statement in else block of ternary operator?
+const findKey = (object, cb) => {
+
+  Object.keys(object).forEach(key => (typeof object[key] === 'object') ? findKey(object[key], cb) : console.log(cb(object[key])) )
+
+}
+*/
+
+
+//callback
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 4 }
+}, x => x.stars === 4)); // => "Akelarre"
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 7 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 7)); // => "Blue Hill"
